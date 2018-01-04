45f89edc33d706a4e0d62976cc173d15c60ba42f
Ethan Lee <hello@ethanl.ee>
2018-01-03 16:06:14 -0800

    Create an index to access Netlify (oops)

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            excerpt(pruneLength: 400)
+            html
+            id
+            frontmatter {
+              templateKey
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()));
+      return Promise.reject(result.errors);
+    }
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
+        context: {}
+      });
+    });
+  });
+};
